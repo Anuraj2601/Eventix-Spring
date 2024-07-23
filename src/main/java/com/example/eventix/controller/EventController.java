f9b9421f17ab827f7cb5ef02456c8a737ee3a01d
DinithiDinushika <dinithidinu689@gmail.com>
2024-07-23 23:05:01 +0530

    Events CRUD

--- src/main/java/com/example/eventix/controller/EventController.java
+++ src/main/java/com/example/eventix/controller/EventController.java
@@ -1,8 +1,11 @@
 package com.example.eventix.controller;
 
+import com.example.eventix.dto.EventDTO;
+import com.example.eventix.dto.ResponseDTO;
 import com.example.eventix.entity.Event;
 import com.example.eventix.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +19,28 @@ public class EventController {
     private EventService eventService;
 
     @GetMapping("/getAllEvents")
-    public String getAllEvents() {
-        return "ALL events";
+    public ResponseEntity<ResponseDTO> getAllEvents() {
+        return ResponseEntity.ok().body(eventService.getAllEvents());
     }
 
     @GetMapping("/getEvent/{event_id}")
-    public String getEvent(){
-        return "1 event";
+    public ResponseEntity<ResponseDTO> getEvent(@PathVariable int event_id){
+        return ResponseEntity.ok().body(eventService.getEvent(event_id));
     }
 
     @PostMapping("/saveEvent")
-    public String saveEvent(){
-        return "save event";
+    public ResponseEntity<ResponseDTO> saveEvent(@RequestBody EventDTO eventDTO){
+        return ResponseEntity.ok().body(eventService.saveEvent(eventDTO));
     }
 
     @PutMapping("/updateEvent")
-    public String updateEvent(){
-        return "update event";
+    public ResponseEntity<ResponseDTO> updateEvent(@RequestBody EventDTO eventDTO){
+        return ResponseEntity.ok().body(eventService.updateEvent(eventDTO));
     }
 
     @DeleteMapping("/deleteEvent/{event_id}")
-    public String deleteEvent(){
-        return "delete event";
+    public ResponseEntity<ResponseDTO> deleteEvent(@PathVariable int event_id){
+        return ResponseEntity.ok().body(eventService.deleteEvent(event_id));
     }
 
 }
--- src/main/java/com/example/eventix/service/EventService.java
+++ src/main/java/com/example/eventix/service/EventService.java
@@ -1,9 +1,19 @@
 package com.example.eventix.service;
 
+import com.example.eventix.dto.EventDTO;
+import com.example.eventix.dto.MeetingDTO;
+import com.example.eventix.dto.ResponseDTO;
+import com.example.eventix.entity.Event;
+import com.example.eventix.entity.Meeting;
 import com.example.eventix.repository.EventRepo;
+import com.example.eventix.util.VarList;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +21,147 @@ public class EventService {
 
     @Autowired
     private EventRepo eventRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ResponseDTO responseDTO;
+
+    public ResponseDTO saveEvent(EventDTO eventDTO) {
+        try{
+            if(eventRepo.existsById(eventDTO.getEvent_id())){
+                responseDTO.setStatusCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("Event already exists");
+                responseDTO.setContent(eventDTO);
+
+            }else{
+                Event savedEvent = eventRepo.save(modelMapper.map(eventDTO, Event.class));
+                EventDTO savedEventDTO = modelMapper.map(savedEvent, EventDTO.class);
+                responseDTO.setStatusCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Event Saved Successfully");
+                responseDTO.setContent(savedEventDTO);
+            }
+
+            return responseDTO;
+
+        }catch(Exception e){
+
+            responseDTO.setStatusCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return responseDTO;
+
+        }
+    }
+
+    public ResponseDTO getAllEvents(){
+        try{
+            List<Event> eventsList = eventRepo.findAll();
+            if(!eventsList.isEmpty()){
+                List<EventDTO> eventDTOList = modelMapper.map(eventsList, new TypeToken<List<EventDTO>>(){}.getType());
+                responseDTO.setStatusCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Retrieved All Events Successfully");
+                responseDTO.setContent(eventDTOList);
+
+            }else{
+                responseDTO.setStatusCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Events Found");
+                responseDTO.setContent(null);
+
+            }
+
+            return responseDTO;
+
+
+        }catch(Exception e){
+
+            responseDTO.setStatusCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return responseDTO;
+
+        }
+    }
+
+    public ResponseDTO getEvent(int event_id){
+        try{
+            if(eventRepo.existsById(event_id)){
+                Event event = eventRepo.findById(event_id).orElse(null);
+                EventDTO eventDTO =  modelMapper.map(event, EventDTO.class);
+                responseDTO.setStatusCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Event Retrieved Successfully");
+                responseDTO.setContent(eventDTO);
+
+            }else{
+                responseDTO.setStatusCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Event Found");
+                responseDTO.setContent(null);
+
+
+            }
+
+            return responseDTO;
+
+        }catch(Exception e){
+            responseDTO.setStatusCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return responseDTO;
+
+        }
+    }
+
+    public ResponseDTO updateEvent(EventDTO eventDTO){
+        try{
+            if(eventRepo.existsById(eventDTO.getEvent_id())){
+                Event updatedEvent =  eventRepo.save(modelMapper.map(eventDTO, Event.class));
+                EventDTO updatedEventDTO = modelMapper.map(updatedEvent, EventDTO.class);
+                responseDTO.setStatusCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Event Updated Successfully");
+                responseDTO.setContent(updatedEventDTO);
+
+
+            }else{
+                responseDTO.setStatusCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Event Found");
+                responseDTO.setContent(null);
+
+            }
+
+            return responseDTO;
+
+        }catch(Exception e){
+            responseDTO.setStatusCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return responseDTO;
+        }
+    }
+
+    public ResponseDTO deleteEvent(int event_id){
+        try{
+            if(eventRepo.existsById(event_id)){
+                eventRepo.deleteById(event_id);
+                responseDTO.setStatusCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Event Deleted Successfully");
+                responseDTO.setContent(null);
+
+
+            }else{
+                responseDTO.setStatusCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Event Found");
+                responseDTO.setContent(null);
+
+            }
+
+            return responseDTO;
+
+        }catch(Exception e){
+            responseDTO.setStatusCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(e);
+            return responseDTO;
+        }
+    }
 }
