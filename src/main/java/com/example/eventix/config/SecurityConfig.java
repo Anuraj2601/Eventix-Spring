d17c108e866e6f3abb9a936dbaa259040898c8a9
anuraj <anurajselvasothy@gmail.com>
2024-07-23 10:14:27 +0530

    Login,registration done

--- src/main/java/com/example/eventix/config/SecurityConfig.java
+++ src/main/java/com/example/eventix/config/SecurityConfig.java
@@ -21,9 +21,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityConfig {
 
     @Autowired
@@ -40,14 +42,18 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request-> request.requestMatchers("/auth/**","/public/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/verify-account").permitAll()
                         .requestMatchers(HttpMethod.PUT,"/regenerate-otp").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/photo").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/static/**").permitAll()
                         .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
                         .requestMatchers("/user/**").hasAnyAuthority("USER")
                         .requestMatchers("/adminuser/**").hasAnyAuthority("ADMIN","USER")
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthFilter, UsernamePasswordAuthenticationFilter.class
                 );
+
         return httpSecurity.build();
     }
 
--- src/main/java/com/example/eventix/controller/UserManagementController.java
+++ src/main/java/com/example/eventix/controller/UserManagementController.java
@@ -5,19 +5,32 @@ import com.example.eventix.dto.ReqRes;
 import com.example.eventix.entity.Users;
 import com.example.eventix.service.UsersManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.example.eventix.constant.Constant.PHOTO_DIRECTORY;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 public class UserManagementController {
+
+
 
     @Autowired
     private UsersManagementService usersManagementService;
 
     @PostMapping("/auth/register")
     public ResponseEntity<ReqRes> register(@RequestBody ReqRes reg) {
+
         return ResponseEntity.ok(usersManagementService.register(reg));
     }
 
@@ -42,7 +55,7 @@ public class UserManagementController {
     }
 
 
-    @PutMapping("/admin/update/{userid}")
+    @PutMapping(value = "/admin/update/{userid}")
     public ResponseEntity<ReqRes> updateUser(@PathVariable Integer userid, @RequestBody Users reqres) {
         return ResponseEntity.ok(usersManagementService.updateUser(userid, reqres));
     }
@@ -65,10 +78,18 @@ public class UserManagementController {
         return ResponseEntity.ok(usersManagementService.verifyAccount(email, otp));
     }
 
-    @PutMapping("/regenerate-otp"  )
+    @PutMapping("/regenerate-otp")
     public ResponseEntity<ReqRes> regenerateOtp(@RequestParam String email) {
         return ResponseEntity.ok(usersManagementService.regenerateOtp(email));
     }
 
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") Integer id ,@RequestParam("file")MultipartFile file) {
+        return ResponseEntity.ok().body(usersManagementService.uploadPhoto(id, file));
+    }
 
+    @GetMapping(path = "static/image/{filename}", produces = {IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE})
+    public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
+    }
 }
