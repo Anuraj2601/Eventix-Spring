bc4d59258d85933db5169ab26a92b4f7f8e7bba8
anuraj <anurajselvasothy@gmail.com>
2024-07-27 18:10:03 +0530

    changes according to cors error

--- src/main/java/com/example/eventix/config/CorsConfig.java
+++ src/main/java/com/example/eventix/config/CorsConfig.java
@@ -15,8 +15,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedOrigins("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+//                        .allowedOrigins("*");
+
             }
         };
     }
