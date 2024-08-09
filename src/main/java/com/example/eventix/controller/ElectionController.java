3ae8dc89ac1daea43e0d01cc2ffd8d6dbe104a64
Dinithi Dinushika <dinithidinu689@gmail.com>
2024-08-10 00:22:13 +0530

    election crud completed

--- src/main/java/com/example/eventix/controller/ElectionController.java
+++ src/main/java/com/example/eventix/controller/ElectionController.java
@@ -1,8 +1,11 @@
 package com.example.eventix.controller;
 
+import com.example.eventix.dto.ElectionDTO;
+import com.example.eventix.dto.ResponseDTO;
 import com.example.eventix.entity.Election;
 import com.example.eventix.service.ElectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,32 +13,32 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173")
 public class ElectionController {
 
-    //@Autowired
-    //private ElectionService electionService;
+    @Autowired
+    private ElectionService electionService;
 
     @GetMapping("/getAllElections")
-    public String getAllElections() {
-        return "all elections";
+    public ResponseEntity<ResponseDTO> getAllElections() {
+        return ResponseEntity.ok().body(electionService.getAllElections());
     }
 
     @GetMapping("/getElection/{election_id}")
-    public String getElection(@PathVariable int election_id) {
-        return "get election";
+    public ResponseEntity<ResponseDTO> getElection(@PathVariable int election_id) {
+        return ResponseEntity.ok().body(electionService.getElection(election_id));
     }
 
     @PostMapping("/saveElection")
-    public String saveElection() {
-        return "save election";
+    public ResponseEntity<ResponseDTO> saveElection(@RequestBody ElectionDTO electionDTO) {
+        return ResponseEntity.ok().body(electionService.saveElection(electionDTO));
     }
 
     @PutMapping("/updateElection/{election_id}")
-    public String updateElection() {
-        return "update election";
+    public ResponseEntity<ResponseDTO> updateElection(@PathVariable int election_id, @RequestBody ElectionDTO electionDTO) {
+        return ResponseEntity.ok().body(electionService.updateElection(election_id, electionDTO));
     }
 
     @DeleteMapping("/deleteElection/{election_id}")
-    public String deleteElection(@PathVariable int election_id) {
-        return "delete election";
+    public ResponseEntity<ResponseDTO> deleteElection(@PathVariable int election_id) {
+        return ResponseEntity.ok().body(electionService.deleteElection(election_id));
     }
 
 
