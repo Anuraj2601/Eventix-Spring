c6ad8f1a074bb4660e2240bec5a0393420f52d88
cusherahkugan <111173285+cusherahkugan@users.noreply.github.com>
2024-11-08 23:06:14 +0530

    board member db updates done using SP

--- src/main/java/com/example/eventix/repository/ElectionRepo.java
+++ src/main/java/com/example/eventix/repository/ElectionRepo.java
@@ -2,6 +2,11 @@ package com.example.eventix.repository;
 
 import com.example.eventix.entity.Election;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.stereotype.Repository;
+
 
 public interface ElectionRepo extends JpaRepository<Election, Integer> {
+    @Procedure(procedureName = "ReleaseElectionResults")
+    void callReleaseElectionResults(Integer electionId);
 }
