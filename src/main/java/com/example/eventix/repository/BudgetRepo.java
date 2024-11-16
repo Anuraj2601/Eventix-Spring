01efcf924812250e3c004201f90369524be34cdb
Dinithi Dinushika <dinithidinu689@gmail.com>
2024-11-16 20:03:48 +0530

    event budget service and controller completed

--- src/main/java/com/example/eventix/repository/BudgetRepo.java
+++ src/main/java/com/example/eventix/repository/BudgetRepo.java
@@ -1,4 +1,8 @@
 package com.example.eventix.repository;
 
-public interface BudgetRepo {
+import com.example.eventix.entity.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BudgetRepo extends JpaRepository<Budget, Integer> {
 }
