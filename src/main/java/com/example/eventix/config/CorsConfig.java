54a5fe9d47128e35eb49bbf15ec4d7684a98940f
anuraj <anurajselvasothy@gmail.com>
2024-12-01 12:04:24 +0530

    change

--- src/main/java/com/example/eventix/config/CorsConfig.java
+++ src/main/java/com/example/eventix/config/CorsConfig.java
@@ -2,10 +2,17 @@ package com.example.eventix.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig {
 
     @Bean
@@ -21,5 +28,18 @@ public class CorsConfig {
                         .allowCredentials(true); // Allow credentials like cookies, authorization headers
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://eventix-18.netlify.app/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
